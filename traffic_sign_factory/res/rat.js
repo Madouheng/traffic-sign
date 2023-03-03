20a93e7acdf3f9c6dfe237a3e295b1605aba432f
Madouheng <99848760+Madouheng@users.noreply.github.com>
2023-03-03 11:44:06 -0600

    Add files via upload

---
+++ traffic_sign_factory/res/rat.js
@@ -0,0 +1,18 @@
+var rats =
+[
+{
+'Better' :      ['Worser','Greater','Smaller'],
+'be' :          ['be','not to be','having fun'],
+'a Samurai' :   ['a Samurai','no one','a Gardener','yourself'],
+'in a Garden' : ['in a Garden','in the Heaven','nowhere','at War'],
+'than' :        ['than']
+},
+{
+'Better' :      ['Worser','Greater','Smaller'],
+'be' :          ['be','not to be','having fun'],
+'a Samurai' :   ['a Samurai','no one','a Gardener','yourself'],
+'in a Garden' : ['in a Garden','in the Heaven','nowhere','at War'],
+'than' :        ['than']
+},
+
+];
