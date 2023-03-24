b50a1903be1bb1358c0921601f5bbbe9ab750cbf
Madouheng <99848760+Madouheng@users.noreply.github.com>
2023-03-24 12:03:43 -0500

    Add files via upload

---
+++ koramadouheng_traffic sign/res/rat.js
@@ -0,0 +1,25 @@
+var rats =
+[
+{
+'Better' :      ['Worser','Greater','Smaller'],
+'be' :          ['be','not to be','having fun'],
+'a Samurai' :   ['a Samurai','no one','a Gardener','yourself'],
+'in a Garden' : ['in a Garden','in the Heaven','nowhere','at War'],
+'than' :        ['than']
+},
+{
+'Better' :      ['slow','Greater','Smaller'],
+'be' :          ['be','not to be','having fun'],
+'a Samurai' :   ['a Samurai','no one','a Gardener','yourself'],
+'in a Garden' : ['in a Garden','in the Heaven','nowhere','at War'],
+'than' :        ['than']
+},
+{
+'traffic' :      ['comprises','pedestrains','vehicles'],
+'ridden' :          ['or','herded','animals'],
+'trains' :   ['and','other','aconveyances','that use'],
+'public' : ['ways','roads','for','transportation'],
+'than' :        ['than']
+},
+
+];
---
+++ koramadouheng_traffic sign/res/ratking.js
@@ -0,0 +1,6 @@
+var ratKing =[
+  'Better be a Sumurai ina Garden than a Gardener in the War',
+  'Don\t keep right',
+  'Give me a brake',
+  'That\'s all Folks'
+];
